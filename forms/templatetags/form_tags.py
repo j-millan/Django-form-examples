dc77bc57ba0e324214059b68f1c5f6d7d9fa0a4b
Jhonattan <millanjhonattan0@gmail.com>
2019-08-05 18:11:52 -0400

    Added a new template tag to show field errors in the sign up form

--- forms/templatetags/form_tags.py
+++ forms/templatetags/form_tags.py
@@ -12,3 +12,10 @@ def input_class(bound_field):
             css_class = 'is-valid'
         
     return 'form-control form-control-sm {0}'.format(css_class)
+
+@register.simple_tag
+def password_error(error, field):
+    base_str = "The {0} field is required"
+    if error == "This field is required.":
+        field_name = "password" if field.name == "password1" else "password confirmation" if field.name == "password2" else field.name
+        return base_str.format(field_name)
